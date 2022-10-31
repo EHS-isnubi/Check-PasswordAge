d3e399688625dea7642fee50ffafacbf0c5cb32e
Louis GAMBART <louis.gambart77270@gmail.com>
2022-10-31 08:41:52 +0100

    Change test order to avoid warning when it has to be a critical Add color in echo

--- Centreon-Alert-PasswordAge.sh
+++ Centreon-Alert-PasswordAge.sh
@@ -3,8 +3,8 @@
 #
 # NAME: Centreon-Alert-PasswordAge.sh
 # AUTHOR: GAMBART Louis
-# DATE: 28/10/2022
-# VERSION 1.4
+# DATE: 31/10/2022
+# VERSION 1.5.1
 #
 # =======================================================
 #
@@ -15,8 +15,16 @@
 # 1.2: Add language control for french based system
 # 1.3: Remove language control and force chage command execution in english
 # 1.4: Add critical exit code for password older than one year
+# 1.5: Change test order to avoid warning when password is older than one year
+# 1.5.1: Add color in echo
 #
 # =======================================================
+
+
+No_Color='\033[0m'
+Red='\033[0;31m'
+Yellow='\033[0;33m'
+Green='\033[0;32m'
 
 
 # if username is not set, set it to root by default
@@ -41,14 +49,16 @@ delta=$(( ($(date -d "$now" +%s) - $(date -d "$out" +%s) )/(60*60*24) ))
 
 
 # if the delta is greater than 180 days, send a warning alert
-if [ "$delta" -gt 180 ]; then
-    echo "WARNING: User $username has a password that is older than 180 days"
-    exit 1
-elif [ "$delta" -gt 365 ]; then
-    echo "CRITICAL: User $username has a password that is older than 365 days"
+if [ "$delta" -gt 365 ]; then
+    echo -e "${Red}CRITICAL${No_Color}: User $username has a password that is older than 365 days"
+    echo "Last password change: $out"
     exit 2
+elif [ "$delta" -gt 180 ]; then
+    echo -e "${Yellow}WARNING${No_Color}: User $username has a password that is older than 180 days"
+    echo "Last password change: $out"
+    exit 1
 else
-    echo "OK: User $username has a password that is not older than 180 days"
+    echo -e "${Green}OK${No_Color}: User $username has a password that is not older than 180 days"
     exit 0
 fi
 
