272efe097cbc415b16be3f89bbb8815ebd53d100
Louis GAMBART <louis.gambart77270@gmail.com>
2022-11-03 09:25:25 +0100

    Rework commentary blocks and script header Add check to run the script only if it runs with root or sudo Add $delta printing in the console

--- Centreon-Alert-PasswordAge.sh
+++ Centreon-Alert-PasswordAge.sh
@@ -1,33 +1,57 @@
 #!/usr/bin/env bash
-# =======================================================
+#==========================================================================================
 #
-# NAME: Centreon-Alert-PasswordAge.sh
-# AUTHOR: GAMBART Louis
-# DATE: 31/10/2022
-# VERSION 1.5.1
+# SCRIPT NAME        :     Centreon-Alert-PasswordAge.sh
 #
-# =======================================================
+# AUTHOR             :     Louis GAMBART
+# CREATION DATE      :     2022.10.27
+# RELEASE            :     v1.5.1
+# USAGE SYNTAX       :     .\Centreon-Alert-PasswordAge.sh <username>
 #
-# CHANGELOG
+# SCRIPT DESCRIPTION :     This script is used to check the password age of a user and return a status code to Centreon.
 #
-# 1.0: Initial version
-# 1.1: Use another bin to secure the script
-# 1.2: Add language control for french based system
-# 1.3: Remove language control and force chage command execution in english
-# 1.4: Add critical exit code for password older than one year
-# 1.5: Change test order to avoid warning when password is older than one year
-# 1.5.1: Add color in echo
+#==========================================================================================
 #
-# =======================================================
+#                 - RELEASE NOTES -
+# v1.0.0  2022.10.27 - Louis GAMBART - Initial version
+# v1.1.0  2022.10.27 - Louis GAMBART - Use another bin to secure the script
+# v1.2.0  2022.10.27 - Louis GAMBART - Add language control for french based system
+# v1.3.0  2022.10.27 - Louis GAMBART - Remove language control and force chage command execution in english
+# v1.4.0  2022.10.28 - Louis GAMBART - Add critical exit code for password older than one year
+# v1.5.0  2022.10.31 - Louis GAMBART - Change test order to avoid warning when password is older than one year
+# v1.5.1  2022.10.31 - Louis GAMBART - Add color in echo
+#
+#==========================================================================================
 
 
-# ====================== COLOR CODE =====================
+#####################
+#                   #
+#  I - COLOR CODES  #
+#                   #
+#####################
 
-No_Color='\033[0m'
-Red='\033[0;31m'
-Yellow='\033[0;33m'
-Green='\033[0;32m'
+No_Color='\033[0m'      # No Color
+Red='\033[0;31m'        # Red
+Yellow='\033[0;33m'     # Yellow
+Green='\033[0;32m '     # Green
 
+
+#####################
+#                   #
+#  II - ROOT CHECK  #
+#                   #
+#####################
+
+if [ "$EUID" -ne 0 ]
+  then echo -e "${Red}Please run as root${No_Color}"
+  exit
+fi
+
+######################
+#                    #
+#  III - CHECK USER  #
+#                    #
+######################
 
 # If any user is specified in the command line, set it as root user
 if [ -z "$1" ]; then
@@ -36,7 +60,6 @@ else
     username="$1"
 fi
 
-
 # If the user provided don't exist, exit the script
 if ! id "$username" >/dev/null 2>&1; then
     echo "User $username does not exist"
@@ -44,30 +67,47 @@ if ! id "$username" >/dev/null 2>&1; then
 fi
 
 
+####################
+#                  #
+#  IV - GET DELTA  #
+#                  #
+####################
+
 # Get the delta, in days, between the current date and the last password change date
 out=$(LANG='' chage -l "$username" | grep "Last password change" | awk '{print $5, $6, $7}' | xargs -I {} date -d "{}" +%Y-%m-%d)
 now=$(date +%Y-%m-%d)
 delta=$(( ($(date -d "$now" +%s) - $(date -d "$out" +%s) )/(60*60*24) ))
 
 
+######################
+#                    #
+#  V - MAIN SCRIPT   #
+#                    #
+######################
+
 # If the delta is greater than 365, exit the script with critical code
 if [ "$delta" -gt 365 ]; then
     echo -e "${Red}CRITICAL${No_Color}: User $username has a password that is older than 365 days"
-    echo "Last password change: $out"
+    echo "Last password change: $out ($delta days ago)"
     exit 2
 # If the delta is greater than 180 days, exit the script with warning code
 elif [ "$delta" -gt 180 ]; then
     echo -e "${Yellow}WARNING${No_Color}: User $username has a password that is older than 180 days"
-    echo "Last password change: $out"
+    echo "Last password change: $out ($delta days ago)"
     exit 1
 # Else, exit the script with ok code
 else
     echo -e "${Green}OK${No_Color}: User $username has a password that is not older than 180 days"
+    echo "Last password change: $out ($delta days ago)"
     exit 0
 fi
 
 
-# ======================= CENTREON ======================
+###################
+#                 #
+#  VI - CENTREON  #
+#                 #
+###################
 
 # 0 = OK
 # 1 = WARNING
